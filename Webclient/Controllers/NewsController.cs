51f7dc071fc9faaef1bfaab84808f4abe4584270
ducnmth1907002 <ducnmth1907002@fpt.edu.vn>
2021-01-15 20:17:52 +0700

    client

---
+++ Webclient/Controllers/NewsController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Webclient.Data;
+using Webclient.Models;
+
+namespace Webclient.Controllers
+{
+    public class NewsController : Controller
+    {
+        private WebclientContext db = new WebclientContext();
+
+        // GET: News
+        public ActionResult Index()
+        {
+            return View(db.News.ToList());
+        }
+
+        // GET: News/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            News news = db.News.Find(id);
+            if (news == null)
+            {
+                return HttpNotFound();
+            }
+            return View(news);
+        }
+    }
+}
